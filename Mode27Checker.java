eafb104ec70f34f9de7c9b872a614b8387066eb2
Mahmoud-Elzankalony <mahmoudelzankelwny85@gmail.com>
2025-11-30 14:09:22 +0200

    Create Mode27Checker.java

---
+++ Mode27Checker.java
@@ -0,0 +1,9 @@
+
+public class Mode27Checker implements Modes{
+    private loadFromFile loader = new loadFromFile();
+    public static  int i =0;
+    @Override
+    public void check() {
+        
+    }
+}
