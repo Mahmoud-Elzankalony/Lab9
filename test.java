cc0a9899ef2f1780c3f4d86bb8aa30dd5afd3fcb
Mahmoud-Elzankalony <mahmoudelzankelwny85@gmail.com>
2025-11-30 21:29:05 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,30 +1,15 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class test
 {
     public  static void main(String[] args) throws IOException
     {
-        /*Mode0Checker checker = new Mode0Checker() ;
-        boolean flag = checker.checkMode0() ;
-        if(flag)
-        {
-            System.out.println("Valid") ;
-        }
-        else
-        {
-            System.out.println("Invalid") ;
-        }
-        */
-
-        Mode3Checker checker = new Mode3Checker();
-        boolean flag = checker.checkMode3();
-        if(flag)
-        {
-            System.out.println("Valid");
-        }
-        else
-        {
-            System.out.println("Invalid");
-        }
+        Scanner scan = new Scanner(System.in);
+        String mode = scan.next();
+        Modes checker =  Factory.getmode(mode);
+           checker.check() ;
+           if(unitCheck.isCheckValidity()==true)
+               System.out.println("Valid");
     }
 }
