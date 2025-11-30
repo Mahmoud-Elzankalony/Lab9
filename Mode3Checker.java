6fbecbd747b9eacd72a21be51831268e677bcd79
Mahmoud-Elzankalony <mahmoudelzankelwny85@gmail.com>
2025-11-30 21:28:36 +0200

    Update Mode3Checker.java

--- Mode3Checker.java
+++ Mode3Checker.java
@@ -1,39 +1,45 @@
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class Mode3Checker {
+public class Mode3Checker implements Modes {
     
     private loadFromFile loader = new loadFromFile();
-    private AtomicBoolean isValid = new AtomicBoolean(true);
+
     
     public Mode3Checker() throws IOException {
         loader.getContent();
     }
     
-    public boolean checkMode3() {
-        isValid.set(true);
-        
+    @Override
+    public void check()  {
+
+
         // Create three threads for rows, columns, and boxes
         Thread rowThread = new Thread(new RowChecker());
         Thread columnThread = new Thread(new ColumnChecker());
         Thread boxThread = new Thread(new BoxChecker());
-        
-        rowThread.start();
-        columnThread.start();
-        boxThread.start();
-        
+
         // Main thread waits for all threads to complete
+        rowThread.start();
         try {
             rowThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        columnThread.start();
+        try {
             columnThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        boxThread.start();
+        try {
             boxThread.join();
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        
-        return isValid.get();
+
     }
-    
     // Thread for checking all rows
     private class RowChecker implements Runnable {
         @Override
@@ -41,8 +47,8 @@ public class Mode3Checker {
             unitCheck check = new unitCheck();
             for (int i = 0; i < 9; i++) {
                 if (!check.isValid(loader.getRow(i))) {
-                    check.func(0, i);
-                    isValid.set(false);
+                    check.printAllsudokuDublicate(0, i);
+
                 }
             }
         }
@@ -55,8 +61,8 @@ public class Mode3Checker {
             unitCheck check = new unitCheck();
             for (int i = 0; i < 9; i++) {
                 if (!check.isValid(loader.getColumn(i))) {
-                    check.func(1, i);
-                    isValid.set(false);
+                    check.printAllsudokuDublicate(1, i);
+
                 }
             }
         }
@@ -69,8 +75,8 @@ public class Mode3Checker {
             unitCheck check = new unitCheck();
             for (int i = 0; i < 9; i++) {
                 if (!check.isValid(loader.getBox(i))) {
-                    check.func(2, i);
-                    isValid.set(false);
+                    check.printAllsudokuDublicate(2, i);
+
                 }
             }
         }
