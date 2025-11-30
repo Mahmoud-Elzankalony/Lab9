dba3c6898e91a2a9743f1b4de4be53d0d401f7a0
Mahmoud-Elzankalony <mahmoudelzankelwny85@gmail.com>
2025-11-30 14:11:04 +0200

    Add 9 Threads for rows

--- Mode27Checker.java
+++ Mode27Checker.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
 
 public class Mode27Checker implements Modes{
     private loadFromFile loader = new loadFromFile();
     public static  int i =0;
     @Override
     public void check() {
-        
+        ArrayList<Thread> threads = new ArrayList<>();
+        boolean flag = true ;
+
+        unitCheck check = new unitCheck();
+
+        for ( i = 0 ; i< 9 ; i ++ )
+        {
+            threads.add(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if(!check.isValid(loader.getRow(i)) )
+                    {
+                        check .printAllsudokuDublicate(0,i) ;
+                    }
+                }
+            }));
+        }
     }
 }
